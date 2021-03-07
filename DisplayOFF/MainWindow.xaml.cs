44d435c9dbd6c69affed2ab03e3512f8bccdc412
Takuto1127 <tiharu717@gmail.com>
2021-03-07 23:41:16 +0900

    CPU使用率が上がる問題を修正
    前のバグ修正前はwhile文の中に特別な処理を加えていなかったので軽かったけど、修正後は処理を加えたので重くなった。
    while文の間隔を変えることで対応。

--- DisplayOFF/MainWindow.xaml.cs
+++ DisplayOFF/MainWindow.xaml.cs
@@ -37,7 +37,8 @@ namespace DisplayOFF
                         PowerOff();
                     }
                 }
-                
+                Thread.Sleep(500);
+
             }
         }));
         
